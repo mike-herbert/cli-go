f0e215103d36166fb79153126e6d4e9dde2a0dc5
Mike Herbert <mikeherbert.dev@gmail.com>
2020-08-23 17:55:13 -0400

    Initial commit, learning how to create CLI in Go

---
+++ cmd/get.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2020  Mike Herbert <mikeherbert.dev@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Does an HTTP get and returns the body",
+	Long:  `cli-go get will attempt an HTTP request and return the body to stdout`,
+	Run: func(cmd *cobra.Command, args []string) {
+		verbose, _ := rootCmd.Flags().GetBool("verbose")
+		if verbose {
+			fmt.Println("Running get command")
+		}
+		url, err := cmd.Flags().GetString("url")
+		if err != nil {
+			// handle error
+		}
+		getURL(url)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().StringP("url", "u", "", "URL to be requested")
+}
+
+func getURL(url string) {
+	if len(url) == 0 {
+		fmt.Println("You must pass in a url if you are enabling the flag --url (-u)")
+		os.Exit(1)
+	}
+	newURL := path.Clean(url)
+	println(newURL)
+}
---
+++ cmd/root.go
@@ -0,0 +1,89 @@
+/*
+Copyright © 2020  Mike Herbert <mikeherbert.dev@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+var Verbose bool
+
+var rootCmd = &cobra.Command{
+	Use:   "cli-go",
+	Short: "cli-go is a tool that I am developing to learn Golang and create CLIs with golang",
+	Long: `cli-go is a tool that I (Mike Herbert) am developing to learn Go and create CLIs with Go.
+The end result is to use Go more frequently in my daily software engineering role. Cobra seems like a great tool to learn to create CLIs`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("In root.Run")
+		fmt.Println(args)
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+}
+
+// Execute ...
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli-go.yaml)")
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".cli-go" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cli-go")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
